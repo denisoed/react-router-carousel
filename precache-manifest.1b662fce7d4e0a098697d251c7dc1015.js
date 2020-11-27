e2950e28009a72185a0c539b7223ea5951b89f84
Denisoed <denisoed@gmail.com>
2020-11-27 20:01:25 +0600

    Updates

---
+++ precache-manifest.1b662fce7d4e0a098697d251c7dc1015.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc7cda6a22833d38a8bfe183f34f515e",
+    "url": "./index.html"
+  },
+  {
+    "revision": "700c91bf74e71513dbd1",
+    "url": "./static/css/main.e370ca25.chunk.css"
+  },
+  {
+    "revision": "357604c30c9216f0cc21",
+    "url": "./static/js/2.3d95031b.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "./static/js/2.3d95031b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "700c91bf74e71513dbd1",
+    "url": "./static/js/main.6d00eb5f.chunk.js"
+  },
+  {
+    "revision": "fe3c1c8f5d355676a547",
+    "url": "./static/js/runtime-main.10e1e4a1.js"
+  }
+]);
